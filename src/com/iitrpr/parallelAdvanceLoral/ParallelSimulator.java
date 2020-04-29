d8df713f07246838baf278e8665fd7981bbe1563
Abhishek Mishra <sabhishekmishra@gmail.com>
2020-04-29 20:05:19 +0530

    Updated 23K data

--- src/com/iitrpr/parallelAdvanceLoral/ParallelSimulator.java
+++ src/com/iitrpr/parallelAdvanceLoral/ParallelSimulator.java
@@ -30,7 +30,7 @@ public class ParallelSimulator {
 			ParallelAdvanceLoral.bestK = ParallelAdvanceLoral.serviceMap.size();
 			// ParallelAdvanceLoral.noOfThreads =
 			// Runtime.getRuntime().availableProcessors()-3;
-			ParallelAdvanceLoral.noOfThreads = 10;
+			ParallelAdvanceLoral.noOfThreads = 5;
 			System.out.println("Total no of threads available are = " + ParallelAdvanceLoral.noOfThreads);
 			double startTime = System.nanoTime();
 
